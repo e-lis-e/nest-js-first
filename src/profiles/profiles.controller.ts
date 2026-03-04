c49e5d84a73cd831471de4523ea20c400014f57e
e-lis-e <elisenovaes10@gmail.com>
2026-03-04 18:34:15 -0300

    Update profiles.controller.ts

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -3,8 +3,12 @@ import { Controller, Get, Query } from '@nestjs/common';
 @Controller('profiles') //decorator
 export class ProfilesController {
   // GET
-  @Get()
+  /*@Get()
   findAll(@Query('age') age: number) {
     return [{ age }];
+  }*/
+  @Get()
+  findAll(@Query('location') location: string) {
+    return [{ location }];
   }
 }
