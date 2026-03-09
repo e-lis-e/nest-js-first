2d167f345f6b94f2317f48f1cdba1f7152c96fa8
e-lis-e <elisenovaes10@gmail.com>
2026-03-09 09:53:37 -0300

    Update profiles.service.ts

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+  private profiles = [
+    {
+      id: randomUUID(),
+      name: 'Claire Waterson',
+      description: 'trying my best to live in here.',
+    },
+    {
+      id: randomUUID(),
+      name: 'Rebecca Carlson',
+      description: 'Hi there!',
+    },
+    {
+      id: randomUUID(),
+      name: 'Josh Darling',
+      description: 'Not in the mood for talking.',
+    },
+  ];
+  findAll() {
+    return this.profiles;
+  }
+
+  findOne(id: string) {
+    return this.profiles.find((profile) => profile.id === id);
+  }
+
+  create(createProfileDto: CreateProfileDto) {
+    const createProfile = {
+      id: randomUUID(),
+      ...createProfileDto,
+    };
+
+    this.profiles.push(createProfileDto);
+    return createProfile;
+  }
+}
